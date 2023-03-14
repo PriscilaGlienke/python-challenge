9a0a342dc7c615520da399fccce9ed3b9575b02f
PriscilaGlienke <priglienke@gmail.com>
2023-03-13 22:24:13 -0400

    changes

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,8 +1,12 @@
+#importing the modules necessary
+
 import os
 import csv
 
+#defining the path of the file to open
 data_csv = os.path.join("Challenge instructions","PyBank/Resources","budget_data.csv")
 
+#defining the veariables
 months = 0
 profit_losses = 0
 previous_pl = 0
@@ -12,19 +16,22 @@ changes = []
 greatest_increase = ["",0]
 greatest_decrease = ["", 999999999]
 
+#reading through the data
 with open(data_csv) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
-    header = next(csvreader)
+    header = next(csvreader) #making sure we skip the first row - header
 
+#calculating the months, profit ans losses and its changes over the entire period in the file by looping through the file
     for row in csvreader:
-        months = months + 1
+        months = months + 1 
         profit_losses = profit_losses + int(row[1])
         change_pl = int(row[1]) - previous_pl
-        changes.append(change_pl)
+        changes.append(change_pl) #adding each change to the list created for all changes
         previous_pl = int(row[1])
 
-        if change_pl > greatest_increase[1]:
+#using if function to define the greatest increase and decrease
+        if change_pl > greatest_increase[1]: 
             greatest_increase[0] = row[0]
             greatest_increase[1] = change_pl
 
@@ -32,24 +39,26 @@ with open(data_csv) as csvfile:
             greatest_decrease[0] = row[0]
             greatest_decrease[1] = change_pl
 
+#calculating the average of the above changes using the sum and the len of the list of changes
+average = round(sum(changes)/len(changes),2)
 
-average = sum(changes)/len(changes)
-
+#printing the results 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {months}")
 print(f"Total: ${profit_losses}")
-print(f"Average Change: ${average:.2f}")
+print(f"Average Change: ${average}")
 print(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
 print(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
 
+#exporting to a text file
 output_file = os.path.join("python-challenge", "PyBank", "analysis", "PyBank_analysis.txt")
 with open(output_file, "w") as datafile:
     writer = csv.writer(datafile)
-    writer.writerow("Financial Analysis")
-    writer.writerow("----------------------------")
-    writer.writerow(f"Total Months: {months}")
-    writer.writerow(f"Total: ${profit_losses}")
-    writer.writerow(f"Average Change: ${average:.2f}")
-    writer.writerow(f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})")
-    writer.writerow(f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})")
+    writer.writerow(["Financial Analysis"])
+    writer.writerow(["----------------------------"])
+    writer.writerow([f"Total Months: {months}"])
+    writer.writerow([f"Total: ${profit_losses}"])
+    writer.writerow([f"Average Change: ${average}"])
+    writer.writerow([f"Greatest Increase in Profits: {greatest_increase[0]} (${greatest_increase[1]})"])
+    writer.writerow([f"Greatest Decrease in Profits: {greatest_decrease[0]} (${greatest_decrease[1]})"])
---
+++ PyPoll/main.py
@@ -0,0 +1,69 @@
+#importing the necessary tools to read the files
+import os
+import csv
+
+#defining the path
+filepath = os.path.join ("Challenge instructions","PyPoll/Resources","election_data.csv")
+
+#defining the variables
+total_votes = 0
+candidates = []
+votes = {}
+
+winner = ""
+winner_votes = 0
+
+#opening the file to read it and looping through it with the appropriate functions
+with open (filepath) as csvfile:
+    csv_reader = csv.reader(csvfile,delimiter=",")
+    next(csv_reader)
+
+    for row in csv_reader:
+        total_votes = total_votes + 1
+        candidate = row[2]
+
+        if candidate not in candidates:
+            candidates.append(candidate)
+            votes[candidate] = 0
+
+        votes[candidate] = votes[candidate] + 1
+
+#printing the analysis
+print("Election Results")
+print("-------------------------")
+print(f"Total Votes: {total_votes}")
+print("-------------------------")
+
+
+#calculating the percentage of votes
+for candidate in candidates:
+    vote_count = votes[candidate]
+    vote_percentage = round(vote_count / total_votes * 100, 3)
+    print(f"{candidate}: {vote_percentage}% ({vote_count})")
+
+print("-------------------------")
+
+#found the formula for the winner with the help of ChatGPT
+winner = max(votes, key=votes.get)
+print(f"Winner: {winner}")
+print("-------------------------")
+
+#exporting the results as a text file
+
+output_path = os.path.join("python-challenge", "PyPoll", "analysis", "PyPoll_analysis.txt")
+with open(output_path, "w") as datafile:
+    writer = csv.writer(datafile)
+    writer.writerow(["Election Results"])
+    writer.writerow(["----------------------------"])
+    writer.writerow([f"Total Votes: {total_votes}"])
+    writer.writerow(["----------------------------"])
+
+    for candidate in candidates:
+        vote_count = votes[candidate]
+        vote_percentage = round(vote_count / total_votes * 100, 3)
+        writer.writerow([f"{candidate}: {vote_percentage}% ({vote_count})"])
+    
+    writer.writerow(["----------------------------"])
+    winner = max(votes, key=votes.get)
+    writer.writerow([f"Winner: {winner}"])
+    writer.writerow(["----------------------------"])
